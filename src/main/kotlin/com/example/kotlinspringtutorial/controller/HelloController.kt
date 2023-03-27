f24e2659b2d44e3c424f1aed34c73565d5e163f2
Hyeonjun Park <phjppo0918@gmail.com>
2023-03-27 17:57:25 +0900

    feat : rest controller sample

---
+++ src/main/kotlin/com/example/kotlinspringtutorial/controller/HelloController.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringtutorial.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping
+    fun hello() : String = "hello"
+}
+
